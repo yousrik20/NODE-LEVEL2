8f5859ccaeef0c3157dd90b417e614747258f844
yousrik20 <https://github.com/yousrik20>
2025-11-26 15:16:10 +0100

    lesson5

--- models/authUser.js
+++ models/authUser.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const authUserSchema = new Schema({
   username: String,
@@ -7,6 +8,13 @@ const authUserSchema = new Schema({
   password: String,
 }); 
 
+
+
+authUserSchema.pre("save", async function (next) {
+ const salt = await bcrypt.genSalt();
+ this.password = await bcrypt.hash(this.password, salt);
+ next();
+});
 const AuthUser = mongoose.model("User", authUserSchema);
 
 module.exports = AuthUser;
