592d659a6cfde00667addb75029064271095df44
yousrik20 <https://github.com/yousrik20>
2025-12-03 11:45:13 +0100

    lesson17

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const Authuser = require("../models/Authuser");
-const bcrypt = require("bcrypt");
-var jwt = require("jsonwebtoken");
+
 var { requireAuth } = require("../middleware/middleware");
 const { checkIfUser } = require("../middleware/middleware");
 const { check, validationResult } = require("express-validator");
+const authController = require("../controllers/authController");
 
 // Level 2
 
 router.get("*", checkIfUser);
 
-router.get("/signout", (req, res) => {
-  res.cookie("jwt", "", { maxAge: 1 });
-  res.redirect("/");
-});
-router.get("/", (req, res) => {
-  res.render("welcome");
-});
+router.get("/signout", authController.get_signout);
 
-router.get("/login", (req, res) => {
-  res.render("auth/login");
-});
+router.get("/login", authController.get_login);
 
-router.get("/signup", (req, res) => {
-  res.render("auth/signup");
-});
+router.get("/signup", authController.get_signup);
 
+router.get("/", authController.get_welcome);
 /* 
 -----old version
 router.post("/signup", (req, res) => {
@@ -51,53 +41,10 @@ router.post(
       "Password must be at least 8 characters with 1 upper case letter and 1 number"
     ).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/),
   ],
-  async (req, res) => {
-    try {
-      // check validation object for errors
-      const objError = validationResult(req);
-      if (objError.errors.length > 0) {
-        return res.json({ arrValidationError: objError.errors });
-      }
-      // check if email already exists
-      const isCurrentEmail = await Authuser.findOne({ email: req.body.email });
-      console.log(isCurrentEmail);
-      if (isCurrentEmail) {
-        return res.json({ existEmail: "this email already used" });
-      }
-      // create new user and login
-      const newUser = await Authuser.create(req.body);
-      var token = jwt.sign({ id: newUser._id }, "shhhhh");
-      res.cookie("jwt", token, { httpOnly: true, maxAge: 86400000 });
-      res.json({ id: newUser._id });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  authController.post_signup
 );
 
-router.post("/login", async (req, res) => {
-  try {
-    const loginUser = await Authuser.findOne({ email: req.body.email });
-    if (loginUser == null) {
-      res.json({notFoundEmail: "this email is not found in DB"});
-    } else {
-      const match = await await bcrypt.compare(
-        req.body.password,
-        loginUser.password
-      );
-      if (match) {
-        console.log("email found in DB & Password match status:", match);
-        var token = jwt.sign({ id: loginUser._id }, "shhhhh");
-        res.cookie("jwt", token, { httpOnly: true, maxAge: 86400000 });
-        res.json({ id: loginUser._id });
-      } else {
-        res.json({ passwordError: "password is incorrect" });
-      }
-    }
-  } catch (error) { 
-    console.log(error);
-  }
-});
+router.post("/login", authController.post_login);
 // GET Requst
 
 router.get("/home", requireAuth, userController.user_index_get);
